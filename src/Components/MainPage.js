8f06bacc735a7e7220500839a29e14813b2d54f7
IoanaMMT <34572741+IoanaMMT@users.noreply.github.com>
2024-04-21 15:54:22 +0100

    first commit

---
+++ src/Components/MainPage.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function MainPage() {
+  return (
+    <>
+      {/* ****************** Navigation ******************** */}
+      <div className="nav_bar">
+        <h3 className="logo">SimplySage</h3>
+        <ul className="nav_list">
+          <li>Skills</li>
+          <li>Projects</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </>
+  );
+}
