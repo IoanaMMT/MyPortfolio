a536cf9acc0e074fe8062d6b7a794bd69d334d99
IoanaMMT <34572741+IoanaMMT@users.noreply.github.com>
2024-04-26 06:57:51 +0100

    basic navbar

--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -1,16 +1,30 @@
 import React from "react";
+import logo from "./Images/Simplysagelogo2.svg";
 
 export default function MainPage() {
   return (
     <>
       {/* ****************** Navigation ******************** */}
       <div className="nav_bar">
-        <h3 className="logo">SimplySage</h3>
-        <ul className="nav_list">
-          <li>Skills</li>
-          <li>Projects</li>
-          <li>Contact</li>
-        </ul>
+        {/* <h3 className="logo">SimplySage</h3> */}
+        <div className="logo">
+          <img
+            src={logo}
+            alt="sage leafs and the word simplysage"
+            width="150"
+            height="50"
+            layout="responsive"
+          />
+        </div>
+        <div className="nav_list_container">
+          <ul className="nav_list">
+            <li>
+              <a href="#">Skills</a>
+            </li>
+            <li>Projects</li>
+            <li>Contact</li>
+          </ul>
+        </div>
       </div>
     </>
   );
