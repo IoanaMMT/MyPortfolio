131e825013ca2888b76627fc667f6e11191f6edd
IoanaMMT <34572741+IoanaMMT@users.noreply.github.com>
2024-04-27 09:45:58 +0100

    Intro page v1

--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import logo from "./Images/Simplysagelogo2.svg";
+import myphoto from "./Images/myphoto.svg";
 
 export default function MainPage() {
   return (
@@ -21,9 +22,31 @@ export default function MainPage() {
             <li>
               <a href="#">Skills</a>
             </li>
-            <li>Projects</li>
-            <li>Contact</li>
+            <li>
+              <a href="#">Project</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
           </ul>
+        </div>
+      </div>
+      {/* **************** Intro Section ******************* */}
+      <div className="intro_section">
+        <div className="text_section">
+          <h3>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </h3>
+        </div>
+        <div className="my_photo">
+          <img
+            src={myphoto}
+            alt="Photo of Ioana"
+            width="542"
+            height="550"
+            layout="responsive"
+          />
         </div>
       </div>
     </>
