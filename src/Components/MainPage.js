4a43ff7f8849cf7e908aba38ad84eb00b7ea5dc1
IoanaMMT <34572741+IoanaMMT@users.noreply.github.com>
2024-04-28 11:38:11 +0100

    split sections background colour

--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -6,39 +6,42 @@ import htmlicon from "./Icons/icons8-html.svg";
 import cssicon from "./Icons/icons8-css.svg";
 import reacticon from "./Icons/icons8-react-js.svg";
 import typescripticon from "./Icons/icons8-typescript.svg";
+import canvaicon from "./Icons/icons8-canva.svg";
 
 export default function MainPage() {
   return (
     <>
       {/* ****************** Navigation ******************** */}
-      <div className="nav_bar">
-        {/* <h3 className="logo">SimplySage</h3> */}
-        <div className="logo">
-          <img
-            src={logo}
-            alt="sage leafs and the word simplysage"
-            width="170"
-            height="50"
-            layout="responsive"
-          />
-        </div>
-        <div className="nav_list_container">
-          <ul className="nav_list">
-            <li>
-              <a href="#">Skills</a>
-            </li>
-            <li>
-              <a href="#">Project</a>
-            </li>
-            <li>
-              <a href="#">Contact</a>
-            </li>
-          </ul>
+      <div className="navigation">
+        <div className="nav_bar">
+          {/* <h3 className="logo">SimplySage</h3> */}
+          <div className="logo">
+            <img
+              src={logo}
+              alt="sage leafs and the word simplysage"
+              width="170"
+              height="50"
+              layout="responsive"
+            />
+          </div>
+          <div className="nav_list_container">
+            <ul className="nav_list">
+              <li>
+                <a href="#">Skills</a>
+              </li>
+              <li>
+                <a href="#">Project</a>
+              </li>
+              <li>
+                <a href="#">Contact</a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
       {/* **************** Intro Section ******************* */}
       <div className="intro_section">
-        <div className="text_section">
+        <div className="intro_text_section">
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua.
@@ -56,33 +59,42 @@ export default function MainPage() {
       </div>
       {/* ********************* Skills ************************* */}
       <div className="skillSection">
-        <div className="text_section">
+        <div className="listedSkills">
+          <div className="card">
+            <img src={htmlicon} alt="html icon" width={80} height={80} />
+            <p>HTML</p>
+          </div>
+          <div className="card">
+            <img src={cssicon} alt="css icon" width={80} height={80} />
+            <p>CSS</p>
+          </div>
+          <div className="card">
+            <img
+              src={typescripticon}
+              alt="typescript icon"
+              width={80}
+              height={80}
+            />
+            <p>Typescript</p>
+          </div>
+          <div className="card">
+            <img src={reacticon} alt="react icon" width={80} height={80} />
+            <p>React</p>
+          </div>
+          <div className="card">
+            <img src={githubicon} alt="github icon" width={80} height={80} />
+            <p>GitHub</p>
+          </div>
+          <div className="card">
+            <img src={canvaicon} alt="github icon" width={80} height={80} />
+            <p>Canva</p>
+          </div>
+        </div>
+        <div className="skills_text_section">
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua.
           </p>
-        </div>
-        <div className="listedSkills">
-          <div>
-            <img src={htmlicon} alt="html icon" />
-            HTML
-          </div>
-          <div>
-            <img src={cssicon} alt="css icon" />
-            CSS
-          </div>
-          <div>
-            <img src={typescripticon} alt="typescript icon" />
-            TypeScript
-          </div>
-          <div>
-            <img src={reacticon} alt="react icon" />
-            React
-          </div>
-          <div>
-            <img src={githubicon} alt="github icon" />
-            GitHub
-          </div>
         </div>
       </div>
     </>
