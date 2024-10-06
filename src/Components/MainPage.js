0d4227dceb13b234e0626c9672eeeadec79608a6
IoanaMMT <34572741+IoanaMMT@users.noreply.github.com>
2024-10-06 17:55:54 +0100

    Hover effects on Skills and Projects

--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -6,23 +6,29 @@ import Button from "react-bootstrap/Button";
 import logo from "./Images/Logo.svg";
 import myphoto from "./Images/myphoto.svg";
 import desktop from "./Images/desk&mobile.svg";
+import bumpchart from "./Images/BumpChart.svg";
+import storypointing from "./Images/Story-pointing-game.svg";
 import githubicon from "./Icons/icons8-github.svg";
 import htmlicon from "./Icons/icons8-html.svg";
 import cssicon from "./Icons/icons8-css.svg";
 import reacticon from "./Icons/icons8-react-js.svg";
 import typescripticon from "./Icons/icons8-typescript.svg";
 import figma from "./Icons/icons8-figma.svg";
+import { FaXTwitter } from "react-icons/fa6";
+import { IoLogoGithub } from "react-icons/io5";
+import { CiLinkedin } from "react-icons/ci";
 import "../App.css";
 
 export default function MainPage() {
   const [open, setOpen] = useState(false);
+  const currentYear = new Date().getFullYear();
 
   return (
     <>
       <body className="entire-page">
         {/* **************************** NAVBAR ********************************** */}
         <nav className="navbar navbar-expand-lg bg-light navbar-light">
-          <div className="container">
+          <div className="container" id="home">
             <img
               src={logo}
               alt="sage leafs and the word simplysage"
@@ -60,7 +66,7 @@ export default function MainPage() {
                     </a>
                   </li>
                   <li className="nav-item">
-                    <a href="#" className="nav-link">
+                    <a href="#contact" className="nav-link">
                       Contact
                     </a>
                   </li>
@@ -92,10 +98,10 @@ export default function MainPage() {
         </section>
         {/* *************************** Skills *************************************** */}
         <section
-          className="text_dark p-5 text-center skills-section"
+          className="text_dark text-center skills-section pt-5 pb-5"
           id="skills"
         >
-          <div className="container">
+          <div className="container pt-5 pb-5">
             <div className="row">
               <div className="col-md-6 order-md-2 align-self-center mb-5 mb-md-0  ">
                 <div className="my-skills-text">
@@ -107,78 +113,90 @@ export default function MainPage() {
                   <div class="col">
                     <div class="card">
                       <div class="card-body skills-icon">
-                        <img
-                          className="img-fluid"
-                          src={htmlicon}
-                          alt="html icon"
-                          width={80}
-                          height={80}
-                        />
+                        <figure>
+                          <img
+                            className="img-fluid pt-3"
+                            src={htmlicon}
+                            alt="html icon"
+                            width={80}
+                            height={80}
+                          />
+                        </figure>
                       </div>
                     </div>
                   </div>
                   <div class="col">
                     <div class="card">
                       <div class="card-body skills-icon">
-                        <img
-                          className="img-fluid"
-                          src={cssicon}
-                          alt="css icon"
-                          width={80}
-                          height={80}
-                        />
+                        <figure>
+                          <img
+                            className="img-fluid pt-3"
+                            src={cssicon}
+                            alt="css icon"
+                            width={80}
+                            height={80}
+                          />
+                        </figure>
                       </div>
                     </div>
                   </div>
                   <div class="col">
                     <div class="card">
                       <div class="card-body skills-icon">
-                        <img
-                          className="img-fluid"
-                          src={typescripticon}
-                          alt="typescript icon"
-                          width={80}
-                          height={80}
-                        />
+                        <figure>
+                          <img
+                            className="img-fluid pt-3"
+                            src={typescripticon}
+                            alt="typescript icon"
+                            width={80}
+                            height={80}
+                          />
+                        </figure>
                       </div>
                     </div>
                   </div>
                   <div class="col">
                     <div class="card">
                       <div class="card-body skills-icon">
-                        <img
-                          className="img-fluid"
-                          src={reacticon}
-                          alt="react icon"
-                          width={80}
-                          height={80}
-                        />
+                        <figure>
+                          <img
+                            className="img-fluid pt-3"
+                            src={reacticon}
+                            alt="react icon"
+                            width={80}
+                            height={80}
+                          />
+                        </figure>
                       </div>
                     </div>
                   </div>
                   <div class="col">
                     <div class="card">
                       <div class="card-body skills-icon">
-                        <img
-                          className="img-fluid"
-                          src={githubicon}
-                          alt="github icon"
-                          width={80}
-                          height={80}
-                        />
+                        <figure>
+                          <img
+                            className="img-fluid pt-3"
+                            src={githubicon}
+                            alt="github icon"
+                            width={80}
+                            height={80}
+                          />
+                        </figure>
                       </div>
                     </div>
                   </div>
                   <div class="col">
                     <div class="card">
                       <div class="card-body skills-icon">
-                        <img
-                          className="img-fluid"
-                          src={figma}
-                          alt="figma icon"
-                          width={80}
-                          height={80}
-                        />
+                        <figure>
+                          <img
+                            className="img-fluid pt-3"
+                            src={figma}
+                            alt="figma icon"
+                            width={80}
+                            height={80}
+                          />
+                        </figure>
                       </div>
                     </div>
                   </div>
@@ -196,8 +214,10 @@ export default function MainPage() {
         <section className="p-5 to-do-list">
           <div className="container">
             <div className="row align-items-centre justify-content-between">
-              <div className="col-md">
-                <img className="desktopimage" src={desktop} alt="Desktop" />
+              <div className="col-md add-shine">
+                <figure>
+                  <img className="desktopimage" src={desktop} alt="Desktop" />
+                </figure>
               </div>
               <div className="col-md-6 p-md-5 pt-lg-0 text-center">
                 <h1 className="pb-3 text-dark">To do list</h1>
@@ -210,25 +230,91 @@ export default function MainPage() {
             </div>
           </div>
         </section>
-        {/* ******************************* Other Project ********************************************** */}
+        {/* ******************************* Transport Trends Chart ********************************************** */}
         <section className="p-5 other-project">
           <div className="container">
             <div className="row align-items-centre justify-content-between">
-              <div className="col-md order-md-2">
-                <img className="desktopimage" src={desktop} alt="Desktop" />
+              <div className="col-md order-md-2 add-shine ">
+                <figure>
+                  <img
+                    className="desktopimage"
+                    src={bumpchart}
+                    alt="Nivo chart"
+                  />
+                </figure>
               </div>
               <div className="col-md-6 p-md-5 text-center">
-                <h1 className="pb-3 text-light">To do list</h1>
+                <h1 className="pb-3 text-light">Transport Trends Chart</h1>
                 <h3 className="text-light">
-                  A responsive and user-friendly to-do list app to efficiently
-                  manage your tasks across all devices.
+                  An interactive and colorful bump chart displaying transport
+                  data over time. Fully responsive for both desktop and mobile,
+                  it offers an intuitive way to explore and customize transport
+                  trends.
                 </h3>
               </div>
             </div>
           </div>
         </section>
-        {/* ************************************ Third Project *********************************************** */}
+        {/* ************************************ Story Pointing Game *********************************************** */}
+        <section className="p-5 to-do-list">
+          <div className="container">
+            <div className="row align-items-centre justify-content-between">
+              <div className="col-md add-shine">
+                <figure>
+                  <img
+                    className="desktopimage"
+                    src={storypointing}
+                    alt="Story Pointing Game"
+                  />
+                </figure>
+              </div>
+              <div className="col-md-6 p-md-5 pt-lg-0 text-center">
+                <h1 className="pb-3 pt-3 text-dark">Story Pointing Game</h1>
+                <h3 className="text-dark">
+                  A collaborative tool for agile teams to estimate user stories
+                  using Fibonacci points. Multiple players can select and reveal
+                  points together. Designed for desktop use, it streamlines the
+                  estimation process.
+                </h3>
+              </div>
+            </div>
+          </div>
+        </section>
       </body>
+      {/* *****************************   Footer     ********************************************* */}
+      <section
+        className="text_dark p-5 text-center skills-section bottom-section"
+        id="contact"
+      >
+        <div className="container">
+          <div className="row justify-content-center align-items-center">
+            {/* Social media icons */}
+            <ul className="social-media-icons d-flex justify-content-center">
+              <li>
+                <a>
+                  <FaXTwitter size={30} />
+                </a>
+              </li>
+              <li className="me-5 ms-5">
+                <a>
+                  <IoLogoGithub size={30} />
+                </a>
+              </li>
+              <li>
+                <a>
+                  <CiLinkedin size={30} />
+                </a>
+              </li>
+            </ul>
+          </div>
+          {/* Copyright section */}
+          <div className="row justify-content-center align-items-center pt-5">
+            <footer style={{ textAlign: "center", padding: "1em" }}>
+              <p>&copy; {currentYear} SimplySage. All rights reserved.</p>
+            </footer>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
