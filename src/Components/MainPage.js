840b69ae6dcf015821a0a714a7a34b5e549695d9
IoanaMMT <34572741+IoanaMMT@users.noreply.github.com>
2024-10-13 14:50:19 +0100

    Changed Showcase section

--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -4,10 +4,11 @@ import Collapse from "react-bootstrap/Collapse";
 import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import logo from "./Images/Logo.svg";
-import myphoto from "./Images/myphoto.svg";
+import myphoto from "./Images/myphoto.jpg";
 import desktop from "./Images/desk&mobile.svg";
 import bumpchart from "./Images/BumpChart.svg";
 import storypointing from "./Images/Story-pointing-game.svg";
+import welcometext from "./Images/WelcomeStyle.svg";
 import githubicon from "./Icons/icons8-github.svg";
 import htmlicon from "./Icons/icons8-html.svg";
 import cssicon from "./Icons/icons8-css.svg";
@@ -77,13 +78,43 @@ export default function MainPage() {
         </nav>
         {/* *************************** SHOWCASE **************************** */}
         <section className="bg-light text_dark pt-5 ps-5 pe-5 pb-3 text-center showcase">
+          <div className="container pb-5">
+            <div className="d-sm-flex align-items-center justify-content-between position-relative">
+              <div className="overlap-text">
+                <img
+                  className="img-fluid pt-3"
+                  src={welcometext}
+                  alt="Intro text"
+                  width={800}
+                  height={800}
+                />
+                <div className="subtext">
+                  <p className="subtext-text">
+                    I am passionate about creating interactive and responsive
+                    web applications
+                  </p>
+                </div>
+              </div>
+              <img
+                className="img-fluid d-none d-sm-block my-image overlap-photo"
+                src={myphoto}
+                alt="Photo of Ioana"
+                layout="responsive"
+              />
+            </div>
+          </div>
+        </section>
+        {/* <section className="bg-light text_dark pt-5 ps-5 pe-5 pb-3 text-center showcase">
           <div className="container">
             <div className="d-sm-flex align-items-center justify-content-between">
               <div>
-                <h3 className="px-md-5">
-                  Welcome to my portfolio! I am passionate about creating
-                  interactive and responsive web applications.
-                </h3>
+                <img
+                  className="img-fluid pt-3"
+                  src={welcometext}
+                  alt="Intro text"
+                  width={1200}
+                  height={1200}
+                />
               </div>
               <img
                 className="img-fluid w-50 d-none d-sm-block my-image"
@@ -95,7 +126,7 @@ export default function MainPage() {
               />
             </div>
           </div>
-        </section>
+        </section> */}
         {/* *************************** Skills *************************************** */}
         <section
           className="text_dark text-center skills-section pt-5 pb-5"
@@ -105,11 +136,12 @@ export default function MainPage() {
             <div className="row">
               <div className="col-md-6 order-md-2 align-self-center mb-5 mb-md-0  ">
                 <div className="my-skills-text">
-                  <h1 className="px-md-5">My Skills</h1>
+                  <h1 className="px-md-5 btn">My Skills</h1>
                 </div>
               </div>
               <div className="col-md-6 order-md-1">
                 <div class="row row-cols-3 g-4">
+                  {/* **** HTML ******* */}
                   <div class="col">
                     <div class="card">
                       <div class="card-body skills-icon">
@@ -125,6 +157,7 @@ export default function MainPage() {
                       </div>
                     </div>
                   </div>
+                  {/* ******  CSS  ****** */}
                   <div class="col">
                     <div class="card">
                       <div class="card-body skills-icon">
@@ -140,6 +173,7 @@ export default function MainPage() {
                       </div>
                     </div>
                   </div>
+                  {/* *******  Typesscript  ******* */}
                   <div class="col">
                     <div class="card">
                       <div class="card-body skills-icon">
@@ -155,6 +189,7 @@ export default function MainPage() {
                       </div>
                     </div>
                   </div>
+                  {/* *********  React  ******** */}
                   <div class="col">
                     <div class="card">
                       <div class="card-body skills-icon">
@@ -170,6 +205,7 @@ export default function MainPage() {
                       </div>
                     </div>
                   </div>
+                  {/* ********  Github  ********** */}
                   <div class="col">
                     <div class="card">
                       <div class="card-body skills-icon">
@@ -185,6 +221,7 @@ export default function MainPage() {
                       </div>
                     </div>
                   </div>
+                  {/* **********  Figma  *********** */}
                   <div class="col">
                     <div class="card">
                       <div class="card-body skills-icon">
@@ -207,8 +244,8 @@ export default function MainPage() {
         </section>
         {/* *************************** Project To Do List *************************************** */}
         <section className="p-5 text-center projects-title" id="projects">
-          <div className="container pt-2">
-            <h1>Projects</h1>
+          <div className="container  pt-2">
+            <h1 className="px-md-5 btn">Projects</h1>
           </div>
         </section>
         <section className="p-5 to-do-list">
@@ -232,7 +269,7 @@ export default function MainPage() {
         </section>
         {/* ******************************* Transport Trends Chart ********************************************** */}
         <section className="p-5 other-project">
-          <div className="container">
+          <div className="container pt-5 pb-5">
             <div className="row align-items-centre justify-content-between">
               <div className="col-md order-md-2 add-shine ">
                 <figure>
@@ -257,7 +294,7 @@ export default function MainPage() {
         </section>
         {/* ************************************ Story Pointing Game *********************************************** */}
         <section className="p-5 to-do-list">
-          <div className="container">
+          <div className="container pt-5 pb-5">
             <div className="row align-items-centre justify-content-between">
               <div className="col-md add-shine">
                 <figure>
@@ -283,7 +320,7 @@ export default function MainPage() {
       </body>
       {/* *****************************   Footer     ********************************************* */}
       <section
-        className="text_dark p-5 text-center skills-section bottom-section"
+        className="text_dark pt-5 text-center skills-section bottom-section"
         id="contact"
       >
         <div className="container">
@@ -308,7 +345,7 @@ export default function MainPage() {
             </ul>
           </div>
           {/* Copyright section */}
-          <div className="row justify-content-center align-items-center pt-5">
+          <div className="row justify-content-center align-items-center pt-4">
             <footer style={{ textAlign: "center", padding: "1em" }}>
               <p>&copy; {currentYear} SimplySage. All rights reserved.</p>
             </footer>
